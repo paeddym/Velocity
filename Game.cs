6a78d18b67eead06956bc26b43a76337e629abf1
Patrik Marosváry <patrikmarosvary@gmail.com>
2025-04-02 20:24:36 +0200

    feat: Finished exercise 1.1

--- Game.cs
+++ Game.cs
@@ -1,4 +1,5 @@
-﻿using OpenTK.Graphics.OpenGL4;
+﻿using System.Threading.Channels;
+using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
@@ -8,6 +9,16 @@ namespace Velocity
     public class Game : GameWindow
     {
         public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) {}
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
     }
 }
 
