08bb7052017bf0fd1c5c17b8fcc9eed379468cdd
Patrik Marosváry <patrikmarosvary@gmail.com>
2025-04-02 19:56:42 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Velocity
+{
+    public class Core
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
