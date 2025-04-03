478d72c5a2fd74eaca834c0195008fe2b310dd4d
Leonhard Schaefler <leonhard.schaefler@gmail.com>
2025-04-03 21:49:19 +0200

    Finish 1.2 Hello Triangle
    coppyd most of the suff from the OpenTK github into the Game Class, somehow i'm to sutpid to get this shit where goes what and why

--- Game.cs
+++ Game.cs
@@ -5,8 +5,50 @@ using OpenTK.Windowing.GraphicsLibraryFramework;
 
 namespace Velocity{
     public class Game : GameWindow {
+        private readonly float[] _vertices =
+        {
+            -0.5f, -0.5f, 0.0f, // Bottom-left vertex
+            0.5f, -0.5f, 0.0f, // Bottom-right vertex
+            0.0f,  0.5f, 0.0f  // Top vertex
+        };
+
+        private int _vertexBufferObject;
+        private int _vertexArrayObject;
+
+        private Shader _shader;
+
         public Game(int width, int height, string title) : 
             base(GameWindowSettings.Default, new NativeWindowSettings() { ClientSize = (width, height), Title = title }) {}
+
+        protected override void OnLoad(){
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            _vertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+
+            _shader = new Shader("shader.vert", "shader.frag");
+            _shader.Use();
+
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+
+            base.OnLoad();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e){
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            _shader.Use();
+            GL.BindVertexArray(_vertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+            SwapBuffers();
+            base.OnRenderFrame(e);
+        }
+
         protected override void OnUpdateFrame(FrameEventArgs e){
             base.OnUpdateFrame(e);
 
@@ -14,5 +56,23 @@ namespace Velocity{
                 Close();
             }
         }
+
+        protected override void OnFramebufferResize(FramebufferResizeEventArgs e){
+            GL.Viewport(0, 0, e.Width, e.Height);
+            base.OnFramebufferResize(e);
+        }
+
+        protected override void OnUnload(){
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+            GL.UseProgram(0);
+
+            GL.DeleteBuffer(_vertexBufferObject);
+            GL.DeleteVertexArray(_vertexArrayObject);
+
+            GL.DeleteProgram(_shader.Handle);
+
+        }
+
     }
 }
---
+++ Shader.cs
@@ -0,0 +1,92 @@
+using OpenTK.Graphics.OpenGL4;
+
+namespace Velocity {
+    public class Shader {
+        public int Handle;
+
+        public Shader(string vertexPath, string fragmentPath) {
+            string VertexShaderSource = File.ReadAllText(vertexPath);
+            string FragmentShaderSource = File.ReadAllText(fragmentPath);
+
+            int VertexShader;
+            int FragmentShader;
+
+            VertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(VertexShader, VertexShaderSource);
+
+            FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(FragmentShader, FragmentShaderSource);
+
+            // Compiling the shaders for some reason the success variable does not wor in this way
+            
+            GL.CompileShader(VertexShader);
+
+            GL.GetShader(VertexShader, ShaderParameter.CompileStatus, out int success);
+            if (success == 0)
+            {
+                string infoLog = GL.GetShaderInfoLog(VertexShader);
+                Console.WriteLine(infoLog);
+            }
+
+            success = 0;
+
+            GL.CompileShader(FragmentShader);
+
+            GL.GetShader(FragmentShader, ShaderParameter.CompileStatus, out success);
+            if (success == 0)
+            {
+                string infoLog = GL.GetShaderInfoLog(FragmentShader);
+                Console.WriteLine(infoLog);
+            }
+            // success does not need to be initialized
+            Handle = GL.CreateProgram();
+
+            GL.AttachShader(Handle, VertexShader);
+            GL.AttachShader(Handle, FragmentShader);
+
+            GL.LinkProgram(Handle);
+
+            GL.GetProgram(Handle, GetProgramParameterName.LinkStatus, out success);
+            if (success == 0)
+            {
+                string infoLog = GL.GetProgramInfoLog(Handle);
+                Console.WriteLine(infoLog);
+            }
+            GL.DetachShader(Handle, VertexShader);
+            GL.DetachShader(Handle, FragmentShader);
+            GL.DeleteShader(FragmentShader);
+            GL.DeleteShader(VertexShader);
+        }
+        public void Use()
+        {
+            GL.UseProgram(Handle);
+        }
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                GL.DeleteProgram(Handle);
+
+                disposedValue = true;
+            }
+        }
+
+        ~Shader()
+        {
+            if (disposedValue == false)
+            {
+                Console.WriteLine("GPU Resource leak! Did you forget to call Dispose()?");
+            }
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
+
