ad4f7e0c737c67913c2ea3eade62dc86f4506644
Leonhard Schaefler <leonhard.schaefler@gmail.com>
2025-04-04 21:47:02 +0200

    1.4 half done clor chanching triangle

--- Game.cs
+++ Game.cs
@@ -3,6 +3,8 @@ using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 
+using System.Diagnostics;
+
 namespace Velocity{
     public class Game : GameWindow {
         //private readonly float[] _vertices =
@@ -12,35 +14,36 @@ namespace Velocity{
         //    0.0f,  0.5f, 0.0f  // Top vertex
         //};
         float[] _vertices = {
-            0.5f,  0.5f, 0.0f,  // top right
+            -0.5f,  -0.5f, 0.0f,  // top right
             0.5f, -0.5f, 0.0f,  // bottom right
-            -0.5f, -0.5f, 0.0f,  // bottom left
-            -0.5f,  0.5f, 0.0f   // top left
+            0.0f, 0.5f, 0.0f,  // bottom left
         };
-        uint[] _indices = {  // note that we start from 0!
-            0, 1, 3,   // first triangle
-            1, 2, 3    // second triangle
-        };
+        //uint[] _indices = {  // note that we start from 0!
+        //    0, 1, 3,   // first triangle
+        //    1, 2, 3    // second triangle
+        //};
 
         private int _vertexBufferObject;
         private int _vertexArrayObject;
-        private int _elementBufferObject;
+        //private int _elementBufferObject;
 
         private Shader _shader;
+        private Stopwatch _timer = new Stopwatch();
 
         public Game(int width, int height, string title) : 
             base(GameWindowSettings.Default, new NativeWindowSettings() { ClientSize = (width, height), Title = title }) {}
 
         protected override void OnLoad(){
+            _timer.Start();
             GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
             _vertexBufferObject = GL.GenBuffer();
             GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
             GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
 
-            _elementBufferObject = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
+            //_elementBufferObject = GL.GenBuffer();
+            //GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+            //GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
 
             _shader = new Shader("shader.vert", "shader.frag");
             _shader.Use();
@@ -48,7 +51,7 @@ namespace Velocity{
             _vertexArrayObject = GL.GenVertexArray();
             GL.BindVertexArray(_vertexArrayObject);
             GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+           // GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
 
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
             GL.EnableVertexAttribArray(0);
@@ -57,13 +60,24 @@ namespace Velocity{
         }
 
         protected override void OnRenderFrame(FrameEventArgs e){
+            base.OnRenderFrame(e);
             GL.Clear(ClearBufferMask.ColorBufferBit);
             _shader.Use();
             GL.BindVertexArray(_vertexArrayObject);
             //GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
-            GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+            
+            double timeValue = _timer.Elapsed.TotalSeconds;
+            float greenValue = (float)Math.Sin(timeValue) / 2.0f + 0.5f;
+            int vertexColorLocation = GL.GetUniformLocation(_shader.Handle, "ourColor");
+            if (vertexColorLocation == -1){
+                Console.WriteLine("Could not find ourColor");
+                Close();
+            }
+            GL.Uniform4(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+            //GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
             SwapBuffers();
-            base.OnRenderFrame(e);
         }
 
         protected override void OnUpdateFrame(FrameEventArgs e){
