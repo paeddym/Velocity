826c064a6ad01262f59e29a28d21434d8edce919
Leonhard Schaefler <leonhard.schaefler@gmail.com>
2025-04-05 02:51:01 +0200

    finish 1.8
    did a lot of things in the wrong order needed to be fixed by GPT, maby some sleep would be good fuck, to stupid to figure this shit out

--- Game.cs
+++ Game.cs
@@ -14,10 +14,47 @@ namespace Velocity{
         //    0.0f,  0.5f, 0.0f  // Top vertex
         //};
         float[] _vertices = {
-            0.5f,  0.5f, 0.0f,    1.0f, 1.0f, 
-            0.5f, -0.5f, 0.0f,      1.0f, 0.0f, 
-            -0.5f, -0.5f, 0.0f,       0.0f, 0.0f,
-            -0.5f, 0.5f, 0.0f,          0.0f, 1.0f
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+            0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+            0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+            0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+            0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
         };
         uint[] _indices = {  // note that we start from 0!
             0, 1, 3,   // first triangle
@@ -26,72 +63,51 @@ namespace Velocity{
 
         private int _vertexBufferObject;
         private int _vertexArrayObject;
-        private int _elementBufferObject;
 
         private Shader _shader;
         private Texture _texture;
         private Texture _texture2;
+        private double _time;
         Matrix4 projection;
+        Matrix4 model;
 
         public Game(int width, int height, string title) : 
             base(GameWindowSettings.Default, new NativeWindowSettings() { ClientSize = (width, height), Title = title }) {}
         protected override void OnLoad(){
-            //ErrorChecker.InitializeGLDebugCallback();
-           Vector4 vec = new Vector4(1.0f, 0.0f, 0.0f, 1.0f);
-           Matrix4 trans = Matrix4.CreateTranslation(1f, 1f, 0.0f);
-           vec *= trans;
-           Console.WriteLine("{0}, {1}, {2}", vec.X, vec.Y, vec.Z);
-
             GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            GL.Enable(EnableCap.DepthTest);
+
+            _shader = new Shader("shader.vert", "shader.frag");
+            _shader.Use();
 
             _texture = new Texture("container.jpg");
             _texture2 = new Texture("awesomeface.png");
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+
 
             _vertexBufferObject = GL.GenBuffer();
             GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
             GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
 
-            _elementBufferObject = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
-
-            _shader = new Shader("shader.vert", "shader.frag");
-            _shader.Use();
-
-            _shader.SetInt("texture1", 0);
-            _shader.SetInt("texture2", 1);
-
-
-            _vertexArrayObject = GL.GenVertexArray();
-            GL.BindVertexArray(_vertexArrayObject);
-
-            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
-
-            // Because there's now 5 floats between the start of the first vertex and the start of the second,
-            // we modify this from 3 * sizeof(float) to 5 * sizeof(float).
-            // This will now pass the new vertex array to the buffer.
             var vertexLocation = GL.GetAttribLocation(_shader.Handle,"aPosition");
             GL.EnableVertexAttribArray(vertexLocation);
             GL.VertexAttribPointer(vertexLocation, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
 
-            // Next, we also setup texture coordinates. It works in much the same way.
-            // We add an offset of 3, since the first vertex coordinate comes after the first vertex
-            // and change the amount of data to 2 because there's only 2 floats for vertex coordinates
             var texCoordLocation = GL.GetAttribLocation(_shader.Handle,"aTexCoord");
             GL.EnableVertexAttribArray(texCoordLocation);
             GL.VertexAttribPointer(texCoordLocation, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
 
-            Matrix4 rotation = Matrix4.CreateRotationZ(MathHelper.DegreesToRadians(90.0f));
-            Matrix4 scale = Matrix4.CreateScale(0.5f, 0.5f, 0.5f);
-            trans = rotation * scale;
 
-            Matrix4 model = Matrix4.CreateRotationX(MathHelper.DegreesToRadians(-55.0f));
+
+            _shader.SetInt("texture1", 0);
+            _shader.SetInt("texture2", 1);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+
+            model = Matrix4.CreateRotationX(MathHelper.DegreesToRadians(-55.0f));
             Matrix4 view = Matrix4.CreateTranslation(0.0f, 0.0f, -3.0f);
             projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-
-            int matlocation = GL.GetUniformLocation(_shader.Handle, "transform");
-            GL.UniformMatrix4(matlocation, true, ref trans);
 
             int modelLocation = GL.GetUniformLocation(_shader.Handle, "model");
             GL.UniformMatrix4(modelLocation, true, ref model);
@@ -107,18 +123,23 @@ namespace Velocity{
 
         protected override void OnRenderFrame(FrameEventArgs e){
             base.OnRenderFrame(e);
+            _time += 32.0 * e.Time;
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
-            GL.Clear(ClearBufferMask.ColorBufferBit);
-
+            _shader.Use();
             GL.BindVertexArray(_vertexArrayObject);
 
             _texture.Use(TextureUnit.Texture0);
             _texture2.Use(TextureUnit.Texture1);
+            
+            model = Matrix4.CreateRotationX((float)MathHelper.DegreesToRadians(_time));
 
-            _shader.Use();
-            GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+            int modelLocation = GL.GetUniformLocation(_shader.Handle, "model");
+            GL.UniformMatrix4(modelLocation, true, ref model);
 
-           // GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
+
             SwapBuffers();
         }
 
