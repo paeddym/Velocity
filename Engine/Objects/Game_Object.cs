30002da32a06dd84cfbc14b7827c862dfe7e0be1
Leonhard Schaefler <leonhard.schaefler@gmail.com>
2025-05-07 20:14:41 +0200

    add Game_Object and Object_Manager class
    they are probaply renamend in future, but the main idee behind them is, that an object contains all the logict to create a verteci with texture and the object manager has them in a list, tracks there positions. and other things. Probaply also the function to compile shader and load shaders and use them. But how this is down needs to be found out in detail

---
+++ Engine/Objects/Game_Object.cs
@@ -0,0 +1,15 @@
+using OpenTK.Graphics.OpenGL4;
+
+namespace Engine {
+    public class GameObject {
+        public GameObject() {
+        }
+
+        public void Use(){
+        }
+
+        ~GameObject(){
+        }
+    }
+}
+
---
+++ Engine/Objects/Object_Manager.cs
@@ -0,0 +1,15 @@
+using OpenTK.Graphics.OpenGL4;
+
+namespace Engine {
+    public class ObjectManager {
+        public ObjectManager() {
+        }
+
+        public void Use(){
+        }
+
+        ~ObjectManager(){
+        }
+    }
+}
+
