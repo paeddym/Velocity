acf76c40818fc8fe4a0c82fdcc883893cb6401cc
Leonhard Schaefler <leonhard.schaefler@gmail.com>
2025-05-08 09:39:21 +0200

    add initial idee on GameObjects and GameObjectManagement The idee is that the GameObjects will contain all the informtaion on how to draw them, meaning things like VBO, VAO, EBO, Shader, Texture, position and so on. With this the ObjectManager will then be able to setup everything to draw the GameObject and Draw it. It also will contain a List of all GameObject objects in the list can be added, deleted, and updated. So in therory, the game logic like colission and other ings can use the Objects in the list to do there stuff

--- Engine/Objects/Game_Object.cs
+++ Engine/Objects/Game_Object.cs
@@ -1,11 +1,51 @@
 using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+// The idee is that an object contains all the information needed to draw an object.
+// The ObjectManger then will use this information to set everything up so that the object can
+// be drawn.
 
 namespace Engine {
     public class GameObject {
-        public GameObject() {
+        String _objectName;
+        Shader _shader;
+        Texture _texture;
+
+        private float _posX = 0f;
+        private float _posY = 0f; 
+        private float _posZ = 0f;
+
+        private float _rotZ = 0f;
+
+        private Vector2 front = new Vector2(0.0f, 0.0f);
+
+        private int VertexBufferObject = 0;
+        private int VertexArrayObject = 0;
+        private int ElementBufferObject = 0;
+
+        public GameObject(String objectName, Shader shader, Texture texture) {
+            this._objectName = objectName;
+            this._shader = shader;
+            this._texture = texture;
+        }
+        public GameObject(String objectName, Shader shader, Texture texture, int vbo,
+                int vao, int ebo) {
+
+            this._objectName = objectName;
+            this._shader = shader;
+            this._texture = texture;
+            this.VertexArrayObject = vao;
+            this.VertexBufferObject = vao;
+            this.ElementBufferObject = ebo;
         }
 
-        public void Use(){
+        public void UpdatePosition(float posX, float posY, float posZ, float rotZ) {
+            this._posX = posX;
+            this._posY = posY;
+            this._posZ = posZ;
+            this._rotZ = rotZ;
         }
 
         ~GameObject(){
--- Engine/Objects/Object_Manager.cs
+++ Engine/Objects/Object_Manager.cs
@@ -1,11 +1,47 @@
 using OpenTK.Graphics.OpenGL4;
 
+// ToDo: Need to figure out on how to init this once an have it reusable in the whole Project!!!
+
+// The idee is that the ObjectManger will use the information stored inside of an GameObject
+// To set everything up in the OpenGL context so the GameObject can be drawn.
+// There is list with all GameObjects, as all objects have a name you can reverenze them with a
+// name probaply something like this ObjectManager objects;
+// Game object = objects.GetGameObject("<ObjectName>");
+// object.UpdatePosition(x, y, z, r);
+
 namespace Engine {
     public class ObjectManager {
-        public ObjectManager() {
-        }
+        // These are the default VBOs, VAOs, EBOs as we only
+        // use Quads so we do not need to create to much
+        // maby this is a good idee maby not.
+        private float[] vertices = {
+            0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
+            0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
+            -0.5f,  0.5f, 0.0f, 0.0f, 1.0f  // top left
+        };
+        private uint[] _indices = {  // note that we start from 0!
+            0, 1, 3,   // first triangle
+            1, 3, 2    // second triangle
+        };
+        private int VertexBufferObject;
+        private int VertexArrayObject;
+        private int ElementBufferObject;
 
-        public void Use(){
+        public ObjectManager() {
+           VertexBufferObject = GL.GenBuffer();
+            VertexArrayObject = GL.GenVertexArray();
+            ElementBufferObject = GL.GenBuffer();
+
+            GL.BindVertexArray(VertexArrayObject);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length *  sizeof(float),
+                    vertices, BufferUsageHint.StaticDraw);
+
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint),
+                    _indices, BufferUsageHint.StaticDraw);
         }
 
         ~ObjectManager(){
